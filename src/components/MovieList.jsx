5d5f5ce4a70cbeea74206921299c4380faad8d90
jadenHong <62584142+jadenHong@users.noreply.github.com>
2020-08-09 10:53:08 -0600

    React: movie-app-view
    modified movieList page using react.js
    it was implemented by javascript.

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,72 +4,77 @@ export const MovieList = () => {
     console.log('5');
     const LIST_LS = 'movieList';
     const [movieList, setMovieList] = useState([]);
-    useEffect(() => {
-        loadList();
+    const [movie, setMovie] = useState({
+        id: '',
+        title: '',
+    });
+    const [inputValue, setInputValue] = useState('');
 
-        // saveToLocal(movieList)
+    useEffect(() => {
+        console.log('data load')
+        loadList();
     }, []);
 
     useEffect(() => {
-        saveToLocal(movieList)
+        console.log('local 저장');
+        saveToLocal(movieList);
+        console.log(movie);
     }, [movieList])
+
+
     const loadList = () => {
         const loadedDate = JSON.parse(localStorage.getItem(LIST_LS));
         console.log(loadedDate)
         if (loadedDate !== null) {
-            console.log(loadedDate)
             setMovieList(loadedDate);
-            console.log(movieList)
         }
 
     }
 
-    const handleSubmit = (e) => {
-        console.log('submit 누름')
-        e.preventDefault();
-        const movieFrom = e.target;
-        const inputMovie = movieFrom.childNodes[0];
-        const inputValue = inputMovie.value;
-        console.log(movieList);
-
-        saveMovie(inputValue);
-        inputMovie.value = '';
-    }
-
-    const saveMovie = (inputValue) => {
-        const movie = {
-            id: movieList.length + 1,
-            title: inputValue
-        }
-        setMovieList([...movieList, movie]);
-        console.log('로컬에 저장')
-    }
-
-    const saveToLocal = (movieList) => {
-        console.log(movieList)
-        localStorage.setItem(LIST_LS, JSON.stringify(movieList));
-    }
     const handleDelete = (e) => {
         console.log(e.target);
         const listNode = e.target.parentNode;
-        console.log(listNode.id);
-        console.log(movieList.filter((movie) => {
-            return movie.id !== parseInt(listNode.id)
-        }));
+        // console.log(listNode.id);
+        // console.log(movieList.filter((movie) => {
+        //     return movie.id !== parseInt(listNode.id)
+        // }));
         const newMovieList = movieList.filter((movie) => {
             return movie.id !== parseInt(listNode.id)
         });
         setMovieList(newMovieList);
     }
 
+    const saveToLocal = (movieList) => {
+        localStorage.setItem(LIST_LS, JSON.stringify(movieList));
+    }
+
+    const handleSubmit = (e) => {
+        console.log('submit');
+        e.preventDefault();
+        setMovieList([...movieList, movie]);
+        setInputValue('');
+
+    }
+
+    const handleChange = (e) => {
+        const inputTitle = e.target.value;
+        console.log(inputTitle);
+        setInputValue(inputTitle);
+        setMovie({
+            id: new Date().valueOf(),
+            title: inputTitle,
+        });
+
+    }
 
     return (
         <Fragment>
             <form className="mymovie-form" onSubmit={handleSubmit}>
-                <input type="text" placeholder="Write a movie to watch" />
+                <input type="text" placeholder="Write a movie to watch" onChange={handleChange} value={inputValue} />
             </form>
             <ul className="movie-list">
-                {movieList.map((movie, index) => <li key={index} id={movie.id}><button onClick={handleDelete} id={index}>❌</button>{movie.title}</li>)}
+                {movieList.map((movie, index) => <li key={index} id={movie.id}><button onClick={handleDelete} id={movie.id}>❌</button>{movie.title}</li>)}
+
             </ul>
         </Fragment>
     )
@@ -113,7 +118,7 @@ export const MovieList = () => {
 
     const saveMovie = (inputValue) => {
         const movie = {
-            id: movieList.length + 1,
+            id: new Date().valueOf(), // 시간을 유닉스코드로 나타낸다. 이것은 유니크한값이다.
             title: inputValue
         }
         setMovieList([...movieList, movie]);
@@ -148,3 +153,79 @@ export const MovieList = () => {
             </ul>
         </Fragment>
     ) */
+
+
+
+
+/* console.log('5');
+const LIST_LS = 'movieList';
+const [movieList, setMovieList] = useState([]);
+useEffect(() => {
+    loadList();
+
+    // saveToLocal(movieList)
+}, []);
+
+useEffect(() => {
+    saveToLocal(movieList)
+}, [movieList])
+const loadList = () => {
+    const loadedDate = JSON.parse(localStorage.getItem(LIST_LS));
+    console.log(loadedDate)
+    if (loadedDate !== null) {
+        console.log(loadedDate)
+        setMovieList(loadedDate);
+        console.log(movieList)
+    }
+
+}
+
+const handleSubmit = (e) => {
+    console.log('submit 누름')
+    e.preventDefault();
+    const movieFrom = e.target;
+    const inputMovie = movieFrom.childNodes[0];
+    const inputValue = inputMovie.value;
+    console.log(movieList);
+
+    saveMovie(inputValue);
+    inputMovie.value = '';
+}
+
+const saveMovie = (inputValue) => {
+    const movie = {
+        id: movieList.length + 1,
+        title: inputValue
+    }
+    setMovieList([...movieList, movie]);
+    console.log('로컬에 저장')
+}
+
+const saveToLocal = (movieList) => {
+    console.log(movieList)
+    localStorage.setItem(LIST_LS, JSON.stringify(movieList));
+}
+const handleDelete = (e) => {
+    console.log(e.target);
+    const listNode = e.target.parentNode;
+    console.log(listNode.id);
+    console.log(movieList.filter((movie) => {
+        return movie.id !== parseInt(listNode.id)
+    }));
+    const newMovieList = movieList.filter((movie) => {
+        return movie.id !== parseInt(listNode.id)
+    });
+    setMovieList(newMovieList);
+}
+
+
+return (
+    <Fragment>
+        <form className="mymovie-form" onSubmit={handleSubmit}>
+            <input type="text" placeholder="Write a movie to watch" />
+        </form>
+        <ul className="movie-list">
+            {movieList.map((movie, index) => <li key={index} id={movie.id}><button onClick={handleDelete} id={index}>❌</button>{movie.title}</li>)}
+        </ul>
+    </Fragment>
+) */
