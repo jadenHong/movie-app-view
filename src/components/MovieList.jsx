60a78ca9c735aa58ae377bbdc08806dc3f505163
jadenHong <62584142+jadenHong@users.noreply.github.com>
2020-08-08 21:21:21 -0600

    React: movie-app-view
    finished of set my movie list section.
    but there is a bug when delete movie list. should fix it soon.

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -50,8 +50,16 @@ export const MovieList = () => {
         localStorage.setItem(LIST_LS, JSON.stringify(movieList));
     }
     const handleDelete = (e) => {
+        console.log(e.target);
         const listNode = e.target.parentNode;
-        console.log(listNode);
+        console.log(listNode.id);
+        console.log(movieList.filter((movie) => {
+            return movie.id !== parseInt(listNode.id)
+        }));
+        const newMovieList = movieList.filter((movie) => {
+            return movie.id !== parseInt(listNode.id)
+        });
+        setMovieList(newMovieList);
     }
 
 
@@ -61,57 +69,82 @@ export const MovieList = () => {
                 <input type="text" placeholder="Write a movie to watch" />
             </form>
             <ul className="movie-list">
-                {movieList.map((movie, index) => <li key={index} id={movie.id}><button onClick={handleDelete}>❌</button>{movie.title}</li>)}
+                {movieList.map((movie, index) => <li key={index} id={movie.id}><button onClick={handleDelete} id={index}>❌</button>{movie.title}</li>)}
             </ul>
         </Fragment>
     )
 }
 
 
-
-
 /* console.log('5');
     const LIST_LS = 'movieList';
-    let movieList = [];
-    let listFromLocal = [];
-
+    const [movieList, setMovieList] = useState([]);
     useEffect(() => {
         loadList();
+
+        // saveToLocal(movieList)
     }, []);
 
+    useEffect(() => {
+        saveToLocal(movieList)
+    }, [movieList])
     const loadList = () => {
+        const loadedDate = JSON.parse(localStorage.getItem(LIST_LS));
+        console.log(loadedDate)
+        if (loadedDate !== null) {
+            console.log(loadedDate)
+            setMovieList(loadedDate);
+            console.log(movieList)
+        }
 
-        listFromLocal = JSON.parse(localStorage.getItem(LIST_LS));
-        console.log(listFromLocal)
     }
 
     const handleSubmit = (e) => {
+        console.log('submit 누름')
         e.preventDefault();
         const movieFrom = e.target;
         const inputMovie = movieFrom.childNodes[0];
         const inputValue = inputMovie.value;
+        console.log(movieList);
+
+        saveMovie(inputValue);
+        inputMovie.value = '';
+    }
+
+    const saveMovie = (inputValue) => {
         const movie = {
-            id: movieList.length,
+            id: movieList.length + 1,
             title: inputValue
         }
-        movieList.push(movie);
-
-        inputMovie.value = '';
-        saveData(movieList);
+        setMovieList([...movieList, movie]);
+        console.log('로컬에 저장')
     }
 
-    const saveData = (movieList) => {
+    const saveToLocal = (movieList) => {
+        console.log(movieList)
         localStorage.setItem(LIST_LS, JSON.stringify(movieList));
     }
+    const handleDelete = (e) => {
+        console.log(e.target);
+        const listNode = e.target.parentNode;
+        console.log(listNode.id);
+        console.log(movieList.filter((movie) => {
+            return movie.id !== parseInt(listNode.id)
+        }));
+        const newMovieList = movieList.filter((movie) => {
+            return movie.id !== parseInt(listNode.id)
+        });
+        setMovieList(newMovieList);
+    }
 
-    console.log(listFromLocal)
+
     return (
         <Fragment>
             <form className="mymovie-form" onSubmit={handleSubmit}>
                 <input type="text" placeholder="Write a movie to watch" />
             </form>
             <ul className="movie-list">
-                {movieList.map((movie, index) => <li key={index}>{movie.title}</li>)}
+                {movieList.map((movie, index) => <li key={index} id={movie.id}><button onClick={handleDelete} id={index}>❌</button>{movie.title}</li>)}
             </ul>
         </Fragment>
     ) */
