051534ced22e7f78536e19da8c9335e65b1a235e
jadenHong <62584142+jadenHong@users.noreply.github.com>
2020-08-10 02:03:08 -0600

    React: movie-app-view
    modified showing movie information with swiper API

--- src/components/Movie2.jsx
+++ src/components/Movie2.jsx
@@ -5,9 +5,29 @@ import SwiperCore, { EffectFade, EffectCube, EffectFlip, EffectCoverflow, Naviga
 
 SwiperCore.use([EffectCoverflow, Navigation, Pagination]);
 export const Movie2 = () => {
+
+
+
     console.log('4');
     const moviesData = data.movies;
-    const handleHover = movie => {
+    /*  const handleHover = movie => {
+         setMovieInfo({
+             key: movie.id,
+             title: movie.title,
+             rating: movie.rating,
+             runtime: movie.runtime,
+             genres: movie.genres.join(', '),// 루프 돌려서 새로운 문자열을 만들어줌
+             summary: movie.summary,
+         })
+     } */
+    const [movieInfo, setMovieInfo] = useState({
+        title: '',
+        rating: 0,
+        runtime: 0,
+        genres: [],
+        summary: '',
+    });
+    const activate = (movie) => {
         setMovieInfo({
             key: movie.id,
             title: movie.title,
@@ -17,14 +37,6 @@ export const Movie2 = () => {
             summary: movie.summary,
         })
     }
-    const [movieInfo, setMovieInfo] = useState({
-        title: '',
-        rating: 0,
-        runtime: 0,
-        genres: [],
-        summary: '',
-    });
-
 
     return (
         <div>
@@ -47,7 +59,12 @@ export const Movie2 = () => {
                 {moviesData.map((movies, index) => {
                     return (
                         <SwiperSlide key={index}>
-                            <img src={movies.medium_cover_image} alt="movies" onMouseOver={() => handleHover(movies)} /> {/*onMouseOver={ handleHover(movies)} 이렇게 사용하면 랜더링이 되자마자 이 함수를 호출하게 되므로 반드시 callBack 함수를 이용해서 사용해야 한다. 그러면 이벤트가 발생햇을 경우에만 콜백함수를 호출 할 수 있다. */}
+
+                            {({ isActive }) => {
+                                isActive && setMovieInfo(movies);
+                                return <img src={movies.medium_cover_image} alt="movies" />
+                            }}
+
                         </SwiperSlide>
                     )
                 })}
@@ -58,8 +75,7 @@ export const Movie2 = () => {
                 <h4>{movieInfo.genres} · {movieInfo.runtime} mins</h4><br></br>
                 <h4>{movieInfo.summary}</h4>
             </div>
-        </div>
+        </div >
 
     )
 }
-
